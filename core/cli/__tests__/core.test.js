5d716bd2d9e9f56561f9f22c90a763075d74fb69
lai_hq@126.com <lai_hq@126.com>
2021-12-17 23:39:34 +0800

    修改versions

--- core/cli/__tests__/core.test.js
+++ core/cli/__tests__/core.test.js
@@ -1,7 +1,13 @@
+/*
+ * @Date: 2021-12-17 22:14:19
+ * @LastEditors: lai_hq@qq.com
+ * @LastEditTime: 2021-12-17 23:39:24
+ * @FilePath: \laihq-web-servef:\code\ohuo\core\cli\__tests__\core.test.js
+ */
 'use strict';
 
 const core = require('..');
 
 describe('core', () => {
-    it('needs tests');
+          it('needs tests');
 });
